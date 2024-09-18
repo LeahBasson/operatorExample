dcce0e253580990ec47f7001cb6d3facecb87fe3
LeahBasson <leahbasson54@gmail.com>
2024-09-18 08:45:40 +0200

    operator example

---
+++ script/code.js
@@ -0,0 +1,23 @@
+// Arithmetic operators: +, - , * , %
+// (modulus), / , ** (exponent), ++, --
+// let numb1 = 2, numb2 = 4, numb3 = 5;
+// console.log(numb1 + numb2 * numb3);
+
+// Assignment operators: =, +=, *=, /=, %=, etc.
+
+// Comparison operators: ==, <, >, <=, >=, !=, ===
+
+// Ternary operator: ? :
+
+// Logical operators: &&, ||, !
+
+// Type of operators: typeof and instanceof
+
+// Bitwise operators (Binary operators): &, |, ~(NOT), ^(XOR), << (left shift), >> (Right shift)
+/*
+2 & 3 =
+2 = 0010
+3 = 0011
+========
+=   0010
+*/
